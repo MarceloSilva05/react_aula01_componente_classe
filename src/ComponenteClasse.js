b28793f16a9ec003e510f255c140021f0ce286ab
MarceloSilva05 <jmarcelosilva.2021@gmail.com>
2021-10-15 01:03:11 -0300

    primeiro commit

---
+++ src/ComponenteClasse.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class ComponenteClasse extends Component{
+    render(){
+        return (
+            <>
+            <h1>Olá mundo React</h1>
+            <p>Esse é meu componente de classe</p>
+            </>
+        )
+    }
+    
+}
